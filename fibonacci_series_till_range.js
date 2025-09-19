7f853ea24782ef92d8d05dd63e52c8ae666c3d1c
khasim <161837558+scriptingmaverick@users.noreply.github.com>
2025-09-19 17:50:09 +0530

    Create fibonacci_series_till_range.js

---
+++ fibonacci_series_till_range.js
@@ -0,0 +1,14 @@
+const endOfRange = 2
+
+let firstTerm = 0
+let secondTerm = 1
+
+let currentTerm = 0
+
+if(endOfRange === 1){
+    console.log(firstTerm)
+}else if(endOfRange === 2){
+    console.log(firstTerm + "\n" + secondTerm)
+}else{
+    
+}
