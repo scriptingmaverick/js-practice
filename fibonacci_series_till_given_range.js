b807ad3b04605575cfa5525df305bf87271ce8d7
khasim <161837558+scriptingmaverick@users.noreply.github.com>
2025-09-19 17:58:20 +0530

    will give series for 1-3

---
+++ fibonacci_series_till_given_range.js
@@ -0,0 +1,28 @@
+const endOfRange = 3
+
+let firstTerm = 0
+let secondTerm = 1
+
+
+if(endOfRange === 1){
+    console.log(firstTerm)
+}else if(endOfRange === 2){
+    console.log(firstTerm + "\n" + secondTerm)
+}else{
+    let currentTerm = 0
+
+    console.log(firstTerm + "\n" + secondTerm)
+    
+    currentTerm += firstTerm+secondTerm
+
+    firstTerm = secondTerm
+
+    secondTerm = currentTerm
+
+    console.log(secondTerm)
+
+    
+
+
+}
+
