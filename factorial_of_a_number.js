91a731a0f51f69dde1a3623bf0381a88d68e0fb7
khasim <161837558+scriptingmaverick@users.noreply.github.com>
2025-09-19 17:34:24 +0530

    will also check for 0 case

--- factorial_of_a_number.js
+++ factorial_of_a_number.js
@@ -1,8 +1,12 @@
 const numberToGetFactorial = 7
 let factorialValue = 1
 
-for(let present_number = 1; present_number <= numberToGetFactorial; present_number++) {
-    factorialValue *= present_number
+if(numberToGetFactorial === 0){
+    console.log(0, "is the factorial")
 }
-
-console.log(factorialValue, "is the factorial")
+else{
+    for(let present_number = 1; present_number <= numberToGetFactorial; present_number++) {
+        factorialValue *= present_number
+    }
+    console.log(factorialValue, "is the factorial")
+}
