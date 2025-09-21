36ae449f0024c9d3e96c60cd21fef1a525ed0b7f
khasim <161837558+scriptingmaverick@users.noreply.github.com>
2025-09-21 13:48:19 +0530

    working for first case
    loops are required in next commit loops will be added instead of statements.

--- finding_hunting_path.js
+++ finding_hunting_path.js
@@ -15,11 +15,225 @@ for(let i=0; i < savannahLand.length; i++) {
 
 const isHuntingPossible = isLionsAvailable && isZebrasAvailable
 
-if(isHuntingPossible) {
-    let shortestPath = 0
-    
-    console.log(shortestPath)
 
-} else{
+if(isHuntingPossible) {
+
+    let isPreyOrPredator = ""
+    let shortestPath = 0
+    let isPreyFound = false
+    let isPredatorFound = false
+
+    let previousPath = Infinity
+
+    let i = 0
+    if ( (isPreyFound && savannahLand[i] === "Z" ) || (isPredatorFound && savannahLand[i] === "L")) {
+        shortestPath = 0
+    } else if(savannahLand[i] === "L" && !isPredatorFound) {
+        isPreyOrPredator = "Predator"
+        isPredatorFound = true
+    } else if(savannahLand[i] === "Z" && !isPreyFound) {
+        isPreyOrPredator = "Prey"
+        isPreyFound = true
+    } else if (savannahLand[i] === " " ) { 
+        shortestPath++
+    }
+
+    if(isPreyFound && isPredatorFound){
+        previousPath = shortestPath < previousPath ? shortestPath : previousPath
+        if(isPreyOrPredator === "Predator"){
+            isPreyFound = false
+        }  else{
+            isPredatorFound = false
+        }
+
+        
+        shortestPath = 0
+    }
+
+    console.log(previousPath,shortestPath,isPredatorFound,isPreyFound,isPreyOrPredator)
+
+
+    i++
+
+    if ( (isPreyFound && savannahLand[i] === "Z" ) || (isPredatorFound && savannahLand[i] === "L")) {
+        
+        shortestPath = 0
+    } else if(savannahLand[i] === "L" && !isPredatorFound) {
+        isPreyOrPredator = "Predator"
+        isPredatorFound = true
+    } else if(savannahLand[i] === "Z" && !isPreyFound) {
+        isPreyOrPredator = "Prey"
+        isPreyFound = true
+    } else if (savannahLand[i] === " " ) { 
+        shortestPath++
+    }
+
+    if(isPreyFound && isPredatorFound){
+        previousPath = shortestPath < previousPath ? shortestPath : previousPath
+        if(isPreyOrPredator === "Predator"){
+            isPreyFound = false
+        }  else{
+            isPredatorFound = false
+        }
+        
+        
+        shortestPath = 0
+    }
+
+    console.log(previousPath,shortestPath,isPredatorFound,isPreyFound,isPreyOrPredator)
+
+
+    i++
+
+    if ( (isPreyFound && savannahLand[i] === "Z" ) || (isPredatorFound && savannahLand[i] === "L")) {
+        
+        shortestPath = 0
+    } else if(savannahLand[i] === "L" && !isPredatorFound) {
+        isPreyOrPredator = "Predator"
+        isPredatorFound = true
+    } else if(savannahLand[i] === "Z" && !isPreyFound) {
+        isPreyOrPredator = "Prey"
+        isPreyFound = true
+    } else if (savannahLand[i] === " " ) { 
+        shortestPath++
+    }
+
+    if(isPreyFound && isPredatorFound){
+        previousPath = shortestPath < previousPath ? shortestPath : previousPath
+        if(isPreyOrPredator === "Predator"){
+            isPreyFound = false
+        }  else{
+            isPredatorFound = false
+        }
+        
+        
+        shortestPath = 0
+    }
+
+    console.log(previousPath,shortestPath,isPredatorFound,isPreyFound,isPreyOrPredator)
+
+
+    i++
+
+    if ( (isPreyFound && savannahLand[i] === "Z" ) || (isPredatorFound && savannahLand[i] === "L")) {
+        
+        shortestPath = 0
+    } else if(savannahLand[i] === "L" && !isPredatorFound) {
+        isPreyOrPredator = "Predator"
+        isPredatorFound = true
+    } else if(savannahLand[i] === "Z" && !isPreyFound) {
+        isPreyOrPredator = "Prey"
+        isPreyFound = true
+    } else if (savannahLand[i] === " " ) { 
+        shortestPath++
+    }
+
+    if(isPreyFound && isPredatorFound){
+        previousPath = shortestPath < previousPath ? shortestPath : previousPath
+        if(isPreyOrPredator === "Predator"){
+            isPreyFound = false
+        }  else{
+            isPredatorFound = false
+        }
+        
+        
+        shortestPath = 0
+    }
+
+    console.log(previousPath,shortestPath,isPredatorFound,isPreyFound,isPreyOrPredator)
+
+
+    i++
+
+    if ( (isPreyFound && savannahLand[i] === "Z" ) || (isPredatorFound && savannahLand[i] === "L")) {
+        
+        shortestPath = 0
+    } else if(savannahLand[i] === "L" && !isPredatorFound) {
+        isPreyOrPredator = "Predator"
+        isPredatorFound = true
+    } else if(savannahLand[i] === "Z" && !isPreyFound) {
+        isPreyOrPredator = "Prey"
+        isPreyFound = true
+    } else if (savannahLand[i] === " " ) { 
+        shortestPath++
+    }
+
+    if(isPreyFound && isPredatorFound){
+        previousPath = shortestPath < previousPath ? shortestPath : previousPath
+        if(isPreyOrPredator === "Predator"){
+            isPreyFound = false
+        }  else{
+            isPredatorFound = false
+        }
+        
+        
+        shortestPath = 0
+    }
+
+    console.log(previousPath,shortestPath,isPredatorFound,isPreyFound,isPreyOrPredator)
+
+
+    i++
+
+    
+    if ( (isPreyFound && savannahLand[i] === "Z" ) || (isPredatorFound && savannahLand[i] === "L")) {
+        
+        shortestPath = 0
+    } else if(savannahLand[i] === "L" && !isPredatorFound) {
+        isPreyOrPredator = "Predator"
+        isPredatorFound = true
+    } else if(savannahLand[i] === "Z" && !isPreyFound) {
+        isPreyOrPredator = "Prey"
+        isPreyFound = true
+    } else if (savannahLand[i] === " " ) { 
+        shortestPath++
+    }
+
+    if(isPreyFound && isPredatorFound){
+        previousPath = shortestPath < previousPath ? shortestPath : previousPath
+        if(isPreyOrPredator === "Predator"){
+            isPreyFound = false
+        }  else{
+            isPredatorFound = false
+        }
+        
+        
+        shortestPath = 0
+    }
+
+    console.log(previousPath,shortestPath,isPredatorFound,isPreyFound,isPreyOrPredator)
+
+
+    i++
+
+    
+    if ( (isPreyFound && savannahLand[i] === "Z" ) || (isPredatorFound && savannahLand[i] === "L")) {
+        
+        shortestPath = 0
+    } else if(savannahLand[i] === "L" && !isPredatorFound) {
+        isPreyOrPredator = "Predator"
+        isPredatorFound = true
+    } else if(savannahLand[i] === "Z" && !isPreyFound) {
+        isPreyOrPredator = "Prey"
+        isPreyFound = true
+    } else if (savannahLand[i] === " " ) { 
+        shortestPath++
+    }
+
+    if(isPreyFound && isPredatorFound){
+        previousPath = shortestPath < previousPath ? shortestPath : previousPath
+        if(isPreyOrPredator === "Predator"){
+            isPreyFound = false
+        }  else{
+            isPredatorFound = false
+        }
+        
+        
+        shortestPath = 0
+    }
+    
+    console.log(previousPath,shortestPath,isPredatorFound,isPreyFound,isPreyOrPredator)
+
+} else {
     console.log(-1)
 }
