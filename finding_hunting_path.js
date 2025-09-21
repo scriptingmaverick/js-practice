c46964f8e2b27ce3bc00c1c3606a22308a50173c
khasim <161837558+scriptingmaverick@users.noreply.github.com>
2025-09-21 11:59:53 +0530

    is will check if there are lions / zebras available in the field or not

--- finding_hunting_path.js
+++ finding_hunting_path.js
@@ -1,6 +1,20 @@
 const savannahLand = "L  ZL Z"
 
+let isLionsAvailable = false
+let isZebrasAvailable = false
 
-let shortestPath = 0
+for(let i=0; i < savannahLand.length; i++) {
+    if( (savannahLand[i] === "L") && !isLionsAvailable){
+        isLionsAvailable = true
+    } else if( (savannahLand[i] === "Z") && !isZebrasAvailable) {
+        isZebrasAvailable = true
+    }else if (isLionsAvailable && isZebrasAvailable){
+        break
+    }
+    console.log(savannahLand[i],isLionsAvailable,isZebrasAvailable)
+}
+
+const isHuntingPossible = isLionsAvailable && isZebrasAvailable
+let shortestPath = isHuntingPossible ? 0 : -1
 
 console.log(shortestPath)
