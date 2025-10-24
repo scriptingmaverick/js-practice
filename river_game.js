ef529403d4bc93feee28bf6edeb7573aa032fef5
khasim <161837558+scriptingmaverick@users.noreply.github.com>
2025-10-24 22:17:00 +0530

    game almost works fine
    have to add levels

--- river_game.js
+++ river_game.js
@@ -1,4 +1,4 @@
-const MAX_RIVER_PATH = 82;
+const MAX_RIVER_PATH = 80;
 const MAX_CROSS_PATH = 68;
 const EMOJIS = ['🐺', '🐑', '🌳'];
 
@@ -18,12 +18,10 @@ let leftSide_animals = '';
 let rightSide_animals = '';
 let user_choices = '';
 
-
 let canIncludeAnimals = false;
-let atEnd = false;
 let deathMsg = '';
 
-let choice_limit = 2;
+let choice_limit = 3;
 
 function delay(x = 2) {
   for (let i = 0; i < 100000000 * x; i++);
@@ -59,6 +57,11 @@ function changeToEmojis(array) {
   return emojis;
 }
 
+function padForBoat(end, text) {
+  end = end + text.length > MAX_RIVER_PATH ? MAX_RIVER_PATH - text.length : end;
+  return text.padStart(end + text.length).padEnd(MAX_RIVER_PATH) + '  ';
+}
+
 function getBoat(boatPosition) {
   let firstPart = `________🕺___`;
   let secondPart = `\\___${generateSequence(4, '_')}____/`;
@@ -67,12 +70,10 @@ function getBoat(boatPosition) {
     firstPart = `___${choices}___🕺___`;
     secondPart = `\\___${generateSequence(choices.length + 2, '_')}____/`;
   }
-  if (atEnd) {
-    boatPosition += 2;
-  }
+
   const boat = [firstPart, secondPart];
-  boat[0] = `${pad(82, boatPosition, boat[0])}`;
-  boat[1] = `${pad(82, boatPosition, boat[1])}`;
+  boat[0] = `${padForBoat(boatPosition, boat[0])}`;
+  boat[1] = `${padForBoat(boatPosition, boat[1])}`;
   return boat;
 }
 
@@ -99,7 +100,7 @@ function checkAndReplace(side = 'left') {
 
 function getMidLines(boatPosition, scene = 0) {
   let boat = getBoat(boatPosition);
-  const line2 = [generateSequence(25), generateSequence(40), generateSequence(25)];
+  const line2 = [generateSequence(25), generateSequence(82), generateSequence(25)];
   const line3 = [getLine3Elem1(), boat[0], pad(25, 1, changeToEmojis(rightSide_animals))];
   const line4 = [generateSequence(25, '-'), boat[1], generateSequence(25, '-')];
 
@@ -107,7 +108,7 @@ function getMidLines(boatPosition, scene = 0) {
     return [line2, line3, line4];
   }
   delay()
-  line2[1] = generateSequence(83);
+  line2[1] = generateSequence(82);
   switch (scene) {
     case 2:
       line2[0] = generateSequence(25 - user_choices.length * 2 - 1) + changeToEmojis(user_choices);
@@ -125,7 +126,6 @@ function getMidLines(boatPosition, scene = 0) {
       line3[1] = boat[0];
       line4[1] = boat[1];
       canIncludeAnimals = false;
-      atEnd = true;
       break;
     case 6:
       const firstPart = 40 - boat[0].trim().length - 1;
@@ -145,10 +145,8 @@ function getMidLines(boatPosition, scene = 0) {
       line2[2] = changeToEmojis(user_choices) + generateSequence(25 - user_choices.length * 2 - 1);
       break;
     case 11:
-      const spacePart = 40 - boat[0].trim().length;
-      line2[1] = generateSequence(spacePart + 40 + user_choices.length) + changeToEmojis(user_choices) + generateSequence(8);
+      line2[1] = pad(82, 82 - user_choices.length * 2 - 8, changeToEmojis(user_choices));
       canIncludeAnimals = true;
-      atEnd = false;
       break;
     case 12:
       boat = getBoat(boatPosition);
@@ -170,42 +168,21 @@ function getMidLines(boatPosition, scene = 0) {
   return [line2, line3, line4];
 }
 
-// user_choices = '12'
-// console.log(getMidLines(3, 13).join('\n'))
-// console.log(getMidLines(3, 14).join('\n'))
-// console.log(getMidLines(3, 15).join('\n'))
-// user_choices = rightSide_animals;
-// rightSide_animals = ''
-// user_choices = '23';
-// rightSide_animals = '23'
-// console.log(getMidLines(66, 9).join('\n'))
 
-// checkAndReplace('right')
-// console.log(getMidLines(66, 10).join('\n'))
-// console.log(getMidLines(66, 11).join('\n'))
-// console.log(getMidLines(66, 12).join('\n'))
-
-function checkingRight(lf, rf, uc) {
-  leftSide_animals = lf;
-  rightSide_animals = rf;
+function check(uc, st, sc) {
   user_choices = uc;
-  let ground = generateGround(66, 9);
-  checkAndReplace('right')
-  for (let index = 10; index < 13; index += 1) {
-    // console.log(ground, '\n i v-> ', index);
-    delay();
-    ground = generateGround(66, index);
-    console.clear();
+  const lines = getMidLines(st, sc)
+  const ar = [];
+  for (let line = 0; line < lines.length; line++) {
+    ar.push(lines[line].join(''))
   }
-  console.log(ground);
+  console.log(ar.join('\n'))
+  user_choices = ''
 }
-
-// checkingRight('12', '23', '2');
 
 function generateGround(boatPosition = 3, scene = 0) {
   const waterGround = generateSequence(40, '☵');
   const lines = getMidLines(boatPosition, scene);
-  // console.log(lines)
   const line2 = lines[0];
   const line3 = lines[1];
   const line4 = lines[2];
@@ -265,60 +242,6 @@ function changeAnimals(isLeftSide = true) {
   return ground;
 }
 
-function generateScene(leftSide, rightSide, choices) {
-  leftSide_animals = leftSide;
-  rightSide_animals = rightSide;
-  user_choices = choices;
-
-  let ground = changeAnimals();
-
-  // start crossing
-  for (let index = 8; index < 18; index += 5) {
-    console.log(ground);
-    delay();
-    ground = generateGround(index, 4);
-    console.clear();
-  }
-  console.log(ground);
-
-
-  // check for logic
-  if (!canContinue()) {
-    delay();
-    ground = generateGround(18, 4);
-    console.clear();
-    console.log(ground);
-    console.log(deathMsg);
-    return;
-  }
-
-  // resume crossing
-  const maxCrossingPath = user_choices.length * 2 < 5 ? MAX_CROSS_PATH - 5 : MAX_CROSS_PATH - 10;
-  console.clear();
-  for (let index = 18; index <= maxCrossingPath; index += 5) {
-    console.log(ground);
-    delay();
-    ground = generateGround(index, 4);
-    console.clear();
-  }
-  console.log(ground);
-
-  const endingPath = 68 - user_choices.length * 2 + (user_choices.length === 2 ? 2 : -1);
-  delay();
-  ground = generateGround(endingPath, 5);
-  console.clear();
-
-  // changing right-side animals
-  for (let i = 6; i <= 8; i++) {
-    console.log(ground);
-    delay();
-    ground = generateGround(endingPath, i);
-    console.clear();
-  }
-  console.log(ground);
-}
-// generateScene('122', '', '12')
-
 function randomStrip(level = 'easy') {
   switch (level) {
     case 'easy':
@@ -331,10 +254,20 @@ function randomStrip(level = 'easy') {
   }
 }
 
-function isValidChoice() {
-  if (user_choices.length > choice_limit) {
-
+function isValidChoice(animalSet) {
+  if (user_choices.length > choice_limit || user_choices.length < 1) {
+    console.log('\n');
+    console.log("  Your combination size must be atleast ", 1, " and atmost ", choice_limit);
+    return false;
   }
+
+  if (!animalSet.split('').sort().join('').includes(user_choices.split('').sort().join(''))) {
+    console.log('\n');
+    console.log("  Your combination must be within this animal range : ", changeToEmojis(animalSet));
+    return false;
+  }
+
+  return true;
 }
 
 function getChoices(animalSet, ground, isLeftSide = true) {
@@ -343,8 +276,13 @@ function getChoices(animalSet, ground, isLeftSide = true) {
     console.log(ground, '\n')
     delay();
     console.log('1. 🐺  \n2. 🐑  \n3. 🌳')
-    user_choices = prompt("enter your combination of animal indices (separated by a comma): ").replaceAll(',', '');
-    // isValidChoice();
+    user_choices = prompt("enter your combination of animal indices (separated by a comma) : ").replaceAll(',', '');
+    if (!isValidChoice(animalSet)) {
+      delay(50);
+      console.log('\n  Please re-enter your choice');
+      delay(40);
+      continue;
+    }
     ground = changeAnimals(isLeftSide);
     console.log(ground, '\n');
 
@@ -361,15 +299,8 @@ function getChoices(animalSet, ground, isLeftSide = true) {
     }
 
     rightSide_animals = animalSet;
-    atEnd = true;
     ground = generateGround(66, 9)
   }
-}
-
-function gameOnSides() {
-  let ground = generateGround();
-  getChoices(ground);
-  return ground;
 }
 
 function crossRiver(ground, direction = 'l-r', start = 8, checkingPosition = 18, checkingScene = 4) {
@@ -417,7 +348,8 @@ function crossRiver(ground, direction = 'l-r', start = 8, checkingPosition = 18,
     }
     console.log(ground);
 
-    const endingPath = 68 - user_choices.length * 2 + (user_choices.length === 2 ? 2 : -1);
+    // const endingPath = 68 - user_choices.length * 2 + (user_choices.length === 2 ? 2 : -1);
+    const endingPath = 68;
     delay();
     ground = generateGround(endingPath, checkingScene);
     console.clear();
@@ -453,35 +385,17 @@ function crossRiver(ground, direction = 'l-r', start = 8, checkingPosition = 18,
   return ground;
 }
 
-// user_choices = '21';
-// rightSide_animals = '331';
-// console.log(changeAnimals())
-
-// user_choices = '13';
-// console.log(changeAnimals(false))
-
-
-// user_choices = '';
-// canIncludeAnimals = false;
-// ground = generateGround(66, 9);
-// console.clear()
-// getChoices('122', ground, false)
-
-// let ground = generateGround(3, 4);
-// console.log(crossRiver(ground));
-// ground = generateGround(66, 4);
-// console.clear();
-// console.log(crossRiver(ground, 'r-l', 61, 56));
-// console.log(crossRiver(ground))
-
 function startGame() {
-  const animalSet = randomStrip();
+  let animalSet = randomStrip();
   leftSide_animals = animalSet;
 
   while (true) {
     let ground = generateGround();
-    ground = getChoices(animalSet, ground);
+    ground = getChoices(leftSide_animals, ground);
     ground = crossRiver(ground);
+    if (deathMsg) {
+      return;
+    }
     if (leftSide_animals.trim() === '') {
       console.log(ground);
       console.log('\n you won.')
@@ -489,6 +403,9 @@ function startGame() {
     }
     ground = getChoices(rightSide_animals, ground, false);
     ground = crossRiver(ground, 'r-l', 61, 56);
+    if (deathMsg) {
+      return;
+    }
     console.log(ground);
   }
 }
