9890a59d1ec48f2cbede76b225163421f7fb6ca3
khasim <161837558+scriptingmaverick@users.noreply.github.com>
2025-10-27 16:44:50 +0530

    add logic to decode nested messages

--- assignments/orion-compression-protocol/ocp_cipher.js
+++ assignments/orion-compression-protocol/ocp_cipher.js
@@ -6,8 +6,7 @@ function addCharsToMsg(char, countOfChar, result) {
   return result + countOfChar + '[' + char + ']';
 }
 
-function encodeData(data, currentIndex, startIndex, countOfChar, result) {
-  console.log(data, startIndex, currentIndex, countOfChar, result)
+function encodeMessage(data, currentIndex, startIndex, countOfChar, result) {
   if (currentIndex === data.length) {
     if (startIndex === currentIndex - 1) {
       return result;
@@ -15,26 +14,69 @@ function encodeData(data, currentIndex, startIndex, countOfChar, result) {
     return addCharsToMsg(data[currentIndex - 1], countOfChar, result);
   }
 
-
   if (data[currentIndex] !== data[currentIndex + 1]) {
     result = addCharsToMsg(data[currentIndex], countOfChar, result);
-    startIndex = currentIndex;
-    countOfChar = 0;
+    return encodeMessage(data, currentIndex + 1, currentIndex, 1, result);
   }
 
-  return encodeData(data, currentIndex + 1, startIndex, countOfChar + 1, result);
+  return encodeMessage(data, currentIndex + 1, startIndex, countOfChar + 1, result);
 }
+
+// function encodeMessage(data, index, startIndex, repeatValue, result) {
+//   if(data[index] === ']'){
+//     return result.repeat(repeatValue);
+//   }
+// }
 
 function encode(data) {
   if (data.length === 1) {
     return data;
   }
 
-  return encodeData(data, 0, 0, 1, '');
+  return encodeMessage(data, 0, 0, 1, '');
+}
+
+// function decodeMessage(data, index, result) {
+//   if (index >= data.length) {
+//     return result;
+//   }
+
+//   const numbers = '0123456789';
+//   if (numbers.includes(data[index])) {
+//     const endIndex = data.slice(index).indexOf('[');
+//     const countOfChar = parseInt(data.slice(index, index + endIndex));
+//     result = result + data[index + endIndex + 1].repeat(countOfChar);
+//     return decodeMessage(data, index + endIndex + 3, result);
+//   }
+
+//   return decodeMessage(data, index + 1, result + data[index]);
+// }
+
+function decodeMessage(data, index, repeatValue, result) {
+  console.log(`data = ${data} , -> index = ${index}, -> data in index = ${data[index]} , -> repeatValue = ${repeatValue} , -> result = ${result}`)
+  if (data[index] === ']' || index >= data.length) {
+    return result.repeat(repeatValue);
+  }
+
+  const numbers = '0123456789';
+  if (numbers.includes(data[index])) {
+    const numberEndIndex = data.indexOf('[');
+    const newRepeatValue = parseInt(data.slice(index, numberEndIndex));
+    const slicedData = data.slice(numberEndIndex + 1);
+    index = data.indexOf(']');
+    result += decodeMessage(slicedData, 0, newRepeatValue, '');
+    return decodeMessage(data, index + 1, repeatValue, result);
+  }
+
+  return decodeMessage(data, index + 1, repeatValue, result + data[index]);
 }
 
 function decode(encodedData) {
+  if (encodedData.length <= 1) {
+    return encodedData;
+  }
 
+  return decodeMessage(encodedData, 0, 0, '');
 }
 
 function checkIndex(array1, array2, index) {
@@ -93,7 +135,7 @@ function testOCP(data, expected, isToEncode = true) {
   const msgExpected = isToEncode ? expected : data;
   const message = createMessage(msgData, result, msgExpected, isToEncode);
   console.log("\n" + message);
-  // return !isToEncode ? console.log('\n') : testOCP(data, expected, false);
+  return !isToEncode ? console.log('\n') : testOCP(data, expected, false);
 }
 
 function testAll() {
@@ -104,4 +146,7 @@ function testAll() {
   testOCP("abbbbcccaa", "a4[b]3[c]2[a]");
 }
 
-testAll();
+// testAll();
+
+// console.log(decode("2[ab3[c]]"));
+// console.log(decode("3[a2[bc]]"));
