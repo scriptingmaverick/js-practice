147876f5f0b2692a96c66aba8c98a3629d4902ac
khasim <161837558+scriptingmaverick@users.noreply.github.com>
2025-11-04 22:50:26 +0530

    create and add logic

---
+++ sorting/selection_sort.js
@@ -0,0 +1,50 @@
+function sort(data) {
+  const sortedData = data.slice();
+  for (let i = 0; i < data.length; i++) {
+    let swapIndex = i;
+    for (let j = i + 1; j < data.length; j++) {
+      if (sortedData[j] < sortedData[swapIndex]) {
+        swapIndex = j;
+      }
+    }
+    if(swapIndex !== i){
+      const temp = sortedData[i];
+      sortedData[i] = sortedData[swapIndex];
+      sortedData[swapIndex] = temp;
+    }
+  }
+
+  return sortedData;
+}
+
+function randomElement(upper, lower) {
+  return lower + Math.floor(Math.random() * (upper - lower));
+}
+
+function randomData(noOfElements) {
+  const data = [];
+  for (let i = 0; i < noOfElements; i++) {
+    data.push(randomElement(1, 100));
+  }
+
+  return data;
+}
+
+function print(message, data) {
+  console.log(`\n${message} : ${data}`);
+}
+
+function heading(text) {
+  console.log(`\n\t\t${text}\n\t\t${'-'.repeat(text.length)}`);
+}
+
+function testSort(noOfElements) {
+  const data = randomData(noOfElements);
+  const sortedData = sort(data);
+  heading('selection sort');
+  print('og data', data);
+  print('sorted data', sortedData);
+}
+
+testSort(5);
+testSort(15);
