85cce5baee890e2a312abcd32fdb2da92a25e151
khasim <khasshaik12@gmail.com>
2025-11-10 23:47:19 +0530

    add some logic for tim sort

--- sorting/tim_sort.js
+++ sorting/tim_sort.js
@@ -1,9 +1,12 @@
-const insertionSort = (array) => {
-  const sortedData = array;
-  for (let i = 0; i < sortedData.length; i++) {
-    if (sortedData[i] < sortedData[i + 1]) {
-      let prev_index = i+1;
-      while (prev_index > 0 && sortedData[prev_index] < sortedData[prev_index - 1]) {
+const insertionSort = (data) => {
+  const sortedData = data;
+  for (let i = 0; i < data.length; i++) {
+    if (sortedData[i] > sortedData[i + 1]) {
+      let prev_index = i + 1;
+      while (
+        prev_index > 0 &&
+        sortedData[prev_index] < sortedData[prev_index - 1]
+      ) {
         const temp = sortedData[prev_index];
         sortedData[prev_index] = sortedData[prev_index - 1];
         sortedData[prev_index - 1] = temp;
@@ -12,12 +15,56 @@ const insertionSort = (array) => {
     }
   }
 };
-const arr = [1, 5, 3, 8, 2, 7, 2];
-insertionSort(arr);
 
-console.log(arr);
-const timSort = (array) => {
-  if (array.length < 32) {
-    insertionSort(array);
+const seperateRuns = (runs, elem, index, array) => {
+  if (runs.length >= 1) {
+    const runsLength = runs.length - 1;
+    if (runs[runsLength][1] === "asc") {
+      console.log(runsLength, runs);
+      const ascArr = runs[runsLength][0];
+      if (elem > ascArr[ascArr.length - 1]) {
+        ascArr.push(elem);
+      } else {
+        runs.push([[elem], elem > array[index + 1] ? "desc" : "asc"]);
+      }
+      return runs;
+    }
+
+    const descArr = runs[runsLength][0];
+    if (elem < descArr[descArr.length - 1]) {
+      descArr.push(elem);
+      if (index === array.length - 1) insertionSort(descArr);
+    } else {
+      insertionSort(descArr);
+      runs.push([[elem], elem > array[index + 1] ? "desc" : "asc"]);
+    }
+    return runs;
   }
+
+  runs.push([[elem], elem > array[index + 1] ? "desc" : "asc"]);
+  return runs;
 };
+
+const timSort = (array) => {
+  // if (array.length < 32) {
+  //  insertionSort(array);
+  // }
+  const runs = array.reduce(seperateRuns, []).map((x) => [x[0]]);
+  console.log(runs);
+};
+
+const randomElement = (upper, lower) =>
+  lower + Math.floor(Math.random() * (upper - lower));
+
+const randomData = (noOfElements) => {
+  const data = [];
+  for (let i = 0; i < noOfElements; i++) {
+    data.push(randomElement(1, 100));
+  }
+
+  return data;
+};
+const arr = randomData(randomElement(0, 10));
+console.log(arr);
+timSort([12, 56, 87, 98, 23, 46, 34, 18, 34, 12]);
+// console.log(arr);
