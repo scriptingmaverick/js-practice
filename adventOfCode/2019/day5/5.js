af070f3397fde2ce5019d8170f5a78c20bc8068f
khasim <khasshaik12@gmail.com>
2025-12-05 08:41:35 +0530

    add logic for day 7 part 1

--- adventOfCode/2019/day5/5.js
+++ adventOfCode/2019/day5/5.js
@@ -1,4 +1,3 @@
-let indexOfInput = 0;
 const applyModes = (modes, arr, pointer, threshold) => {
   const indices = [];
   for (let i = 1; i <= threshold; i++) {
@@ -13,18 +12,14 @@ const isZero = (value) => value === 0;
 const isNotZero = (value) => !isZero(value);
 const areEqual = (number1, number2) => number1 === number2;
 const isLessThan = (number1, number2) => number1 < number2;
-const storeInput = (inputs, arr, modes, result) => {
+const storeInput = (input, arr, modes, result) => {
   const index = applyModes(modes, arr, result.pointerPos, 1)[0];
-  arr[index] = inputs[indexOfInput++];
+  arr[index] = input;
   result.pointerPos += 2;
 };
-const showData = (arr, modes, result, storage) => {
+const showData = (arr, modes, result) => {
   const index = applyModes(modes, arr, result.pointerPos, 1);
-  if (typeof storage === "object") {
-    storage[0] = arr[index];
-  } else {
-    console.log(arr[index]);
-  }
+  console.log(arr[index]);
   result.pointerPos += 2;
 };
 const jmp = (func, memory, modes, result) => {
@@ -45,14 +40,13 @@ const perform = (func, memory, modes, result) => {
   result.pointerPos += 4;
 };
 
-export const sprint = (corruptedMemory, inputs = [5], storage = 0) => {
+export const sprint = (corruptedMemory, input = 5) => {
   const rawMemory = corruptedMemory.split(",");
   const execute = {
     1: (result, memory, modes) => perform(add, memory, modes, result),
     2: (result, memory, modes) => perform(mul, memory, modes, result),
-    3: (result, memory, modes) => storeInput(inputs, memory, modes, result),
-    4: (result, memory, modes, storage) =>
-      showData(memory, modes, result, storage),
+    3: (result, memory, modes) => storeInput(input, memory, modes, result),
+    4: (result, memory, modes) => showData(memory, modes, result),
     5: (result, memory, modes) => jmp(isNotZero, memory, modes, result),
     6: (result, memory, modes) => jmp(isZero, memory, modes, result),
     7: (result, memory, modes) => algebra(isLessThan, memory, modes, result),
@@ -71,53 +65,15 @@ export const sprint = (corruptedMemory, inputs = [5], storage = 0) => {
   while (result.pointerPos < memory.length && result.canContinue) {
     const cmd = memory[result.pointerPos].toString().padStart(5, "0");
     const modes = cmd.slice(0, 3).split("").reverse();
-    execute[cmd.at(-1)](result, memory, modes, storage);
+    execute[cmd.at(-1)](result, memory, modes);
   }
-
-  indexOfInput = 0;
-  // console.log("mem : ", memory, "\ninp :", inputs, "\nstorage : ", storage);
-  return storage[0];
 };
 
 const input = Deno.readTextFileSync("input.txt");
-// sprint(input);
+sprint(input);
 
 // sprint('3,9,8,9,10,9,4,9,99,-1,8')
 // sprint('3,9,7,9,10,9,4,9,99,-1,8')
 // sprint('3,3,1108,-1,8,3,4,3,99')
 // sprint('3,3,1107,-1,8,3,4,3,99')
 // sprint('3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99')
-
-// const r1 = [];
-// sprint(
-//   "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0",
-//   [1, 0],
-//   r1,
-// );
-// console.log('r1 : ',r1)
-// sprint(
-//   "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0",
-//   [0, r1[0]],
-//   r1,
-// );
-// console.log('r1 : ',r1)
-// sprint(
-//   "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0",
-//   [4, r1[0]],
-//   r1,
-// );
-// console.log('r1 : ',r1)
-// sprint(
-//   "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0",
-//   [3, r1[0]],
-//   r1,
-// );
-// console.log('r1 : ',r1)
-// // console.log('r1 : ',r1)
-// sprint(
-//   "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0",
-//   [2, r1[0]],
-//   r1,
-// );
-
-// console.log('r1 : ',r1);
--- adventOfCode/2019/day7/7.js
+++ adventOfCode/2019/day7/7.js
@@ -1,18 +1,102 @@
-import { sprint } from "../day5/5.js";
+let inputsIndex = 0;
+const applyModes = (modes, arr, pointer, threshold) => {
+  const indices = [];
+  for (let i = 1; i <= threshold; i++) {
+    indices.push(+modes[i - 1] === 0 ? arr[pointer + i] : pointer + i);
+  }
+  return indices;
+};
+
+const add = (addend, augend) => addend + augend;
+
+const mul = (multiplier, multiplicand) => multiplier * multiplicand;
+
+const isZero = (value) => value === 0;
+
+const isNotZero = (value) => !isZero(value);
+
+const areEqual = (number1, number2) => number1 === number2;
+
+const isLessThan = (number1, number2) => number1 < number2;
+
+const storeInput = (arr, modes, result) => {
+  const index = applyModes(modes, arr, result.pointerPos, 1)[0];
+  arr[index] = result.inputs[inputsIndex++];
+  result.pointerPos += 2;
+};
+
+const showData = (arr, modes, result) => {
+  const index = applyModes(modes, arr, result.pointerPos, 1);
+  result.storage = arr[index];
+  result.pointerPos += 2;
+};
+
+const jmp = (func, memory, modes, result) => {
+  const indices = applyModes(modes, memory, result.pointerPos, 2);
+  result.pointerPos = func(memory[indices[0]])
+    ? memory[indices[1]]
+    : result.pointerPos + 3;
+};
+
+const algebra = (func, arr, modes, result) => {
+  const indices = applyModes(modes, arr, result.pointerPos, 3);
+  arr[indices[2]] = func(arr[indices[0]], arr[indices[1]]) ? 1 : 0;
+  result.pointerPos += 4;
+};
+
+const perform = (func, memory, modes, result) => {
+  const indices = applyModes(modes, memory, result.pointerPos, 3);
+  memory[indices[2]] = func(memory[indices[0]], memory[indices[1]]);
+  result.pointerPos += 4;
+};
+
+export const sprint = (corruptedMemory, inputs) => {
+  const storage = [];
+  const rawMemory = corruptedMemory.split(",");
+  const execute = {
+    1: (result, memory, modes) => perform(add, memory, modes, result),
+    2: (result, memory, modes) => perform(mul, memory, modes, result),
+    3: (result, memory, modes) => storeInput(memory, modes, result),
+    4: (result, memory, modes) => showData(memory, modes, result),
+    5: (result, memory, modes) => jmp(isNotZero, memory, modes, result),
+    6: (result, memory, modes) => jmp(isZero, memory, modes, result),
+    7: (result, memory, modes) => algebra(isLessThan, memory, modes, result),
+    8: (result, memory, modes) => algebra(areEqual, memory, modes, result),
+    9: (result) => (result.canContinue = false),
+  };
+
+  const memory = rawMemory.map((x) => parseInt(x));
+
+  // console.log("mem : ", memory, "\ninp :", inputs, "\nstorage : ", storage);
+  const result = {
+    pointerPos: 0,
+    canContinue: true,
+    inputs,
+    storage,
+  };
+
+  while (result.pointerPos < memory.length && result.canContinue) {
+    const cmd = memory[result.pointerPos].toString().padStart(5, "0");
+    const modes = cmd.slice(0, 3).split("").reverse();
+    execute[cmd.at(-1)](result, memory, modes);
+  }
+  inputsIndex = 0;
+  return result.storage;
+};
 
 const amplify = (memory, signal, prevResult) => {
   for (let i = 0; i < signal.length; i++) {
     const mem = memory.slice();
-    prevResult = sprint(mem, [signal[i], prevResult], []);
+    const inputs = [+signal[i], prevResult];
+    prevResult = sprint(mem, inputs);
   }
-  
+
   return prevResult;
 };
 
 const ampliyCircuit = (rawMemory) => {
   const signals = Deno.readTextFileSync("permutations.txt").split("\n")
     .map((x) => x.split(""));
-
   const thrusterOutputs = [];
   for (let i = 0; i < signals.length; i++) {
     thrusterOutputs.push(amplify(rawMemory, signals[i], 0));
@@ -21,8 +105,36 @@ const ampliyCircuit = (rawMemory) => {
   console.log(thrusterOutputs, Math.max(...thrusterOutputs));
 };
 
-console.log(
-  ampliyCircuit(
-    "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0",
-  ),
-);
+const input = Deno.readTextFileSync("input.txt");
+// console.log(input )
+ampliyCircuit(input);
+
+// ampliyCircuit(
+//   "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0",
+// );
+
+// ampliyCircuit(
+//   "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0",
+// );
+
+// ampliyCircuit(
+//   "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0",
+// );
+
+// console.log(amplify(
+//   "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0",
+//   ["0", "1", "2", "3", "4"],
+//   0,
+// ));
+
+// console.log(amplify(
+//   "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0",
+//   ["4", "3", "2", "1", "0"],
+//   0,
+// ));
+
+// console.log(amplify(
+//   "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0",
+//   ["1", "0", "4", "3", "2"],
+//   0,
+// ));
