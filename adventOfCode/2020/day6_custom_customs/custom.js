e6fe5232b483e76f04a010350be99f308ee1e680
khasim <khasshaik12@gmail.com>
2025-12-25 10:45:56 +0530

    adds logic for part 1

--- adventOfCode/2020/day6_custom_customs/custom.js
+++ adventOfCode/2020/day6_custom_customs/custom.js
@@ -1,6 +1,43 @@
-const main = () => {
-  const input = Deno.readTextFileSync('input.txt').split('\n');
-  return input
-}
+const parse = (data) =>
+  data.split("\r\n\r\n").map((x) => x.split("\r\n"));
+  // data.split("\n\n").map((x) => x.split("\n"));
 
-console.log(main())
+const SummategroupAnswers = (groupsData) => {
+  const groups = parse(groupsData);
+  let sum = 0;
+  for (const group of groups) {
+    const distictQuestions = {};
+    for (const persons of group) {
+      for (let i = 0; i < persons.length; i++) {
+        distictQuestions[persons[i]] = true;
+      }
+    }
+    // console.log(distictQuestions);
+    sum += Object.keys(distictQuestions).length;
+  }
+
+  return sum;
+};
+
+const main = () => {
+  const input = Deno.readTextFileSync("input.txt");
+  const example = `abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b`;
+
+  return SummategroupAnswers(input);
+};
+
+console.log(main());
