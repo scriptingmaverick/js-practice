1cdaec745fb7c733380db80a322c7b023fee9f06
khasim <khasshaik12@gmail.com>
2025-12-25 17:04:16 +0530

    adds logic for part 2

--- adventOfCode/2020/day8_handheld_halt/handheld_halt.js
+++ adventOfCode/2020/day8_handheld_halt/handheld_halt.js
@@ -19,24 +19,61 @@ const runComputer = (program, pointer = 0) => {
     pointer,
     accumulator: 0,
     views: {},
+    isEndAbruptly: false,
   };
-
-  while (true) {
+  // console.log(program);
+  // return
+  // prompt();
+  while (result.pointer < program.length) {
     const instruction = program[result.pointer];
     const key = instruction + " " + result.pointer;
-    if (key in result.views) break;
+    if (key in result.views) {
+      result.isEndAbruptly = true;
+      break;
+    }
+    // console.log(result.pointer);
     const [cmd, destination] = instruction.split(" ");
     execute[cmd](result, destination);
     result.views[key] = true;
+  }
 
-    // console.log(result.pointer);
-    // prompt();
+  return result;
+};
+
+const part1 = (program) => runComputer(program).accumulator;
+
+const getCombinations = (program) => {
+  const combinations = [];
+  const newCmdOf = {
+    jmp: ["nop", true],
+    nop: ["jmp", true],
+    acc: ["acc", false],
+  };
+
+  for (let i = 0; i < program.length; i++) {
+    const [cmd, value] = program[i].split(" ");
+    const [newCmd, canAddToProgram] = newCmdOf[cmd];
+    if (canAddToProgram) {
+      const newProg = program.slice();
+      newProg[i] = newCmd + " " + value;
+      combinations.push(newProg);
+    }
+  }
+
+  return combinations;
+};
+
+const part2 = (program) => {
+  const combinations = getCombinations(program);
+  let result = runComputer(combinations.pop());
+  while (result.isEndAbruptly) {
+    result = runComputer(combinations.pop());
   }
 
   return result.accumulator;
 };
 
-const main = () => {
+const main = (fn = part1) => {
   const input = Deno.readTextFileSync("input.txt").split("\r\n");
   const example = `nop +0
 acc +1
@@ -47,7 +84,8 @@ acc -99
 acc +1
 jmp -4
 acc +6`.split("\n");
-  return runComputer(input);
+
+  return fn(input);
 };
 
-console.log(main());
+console.log("acc -> ", main(part2));
