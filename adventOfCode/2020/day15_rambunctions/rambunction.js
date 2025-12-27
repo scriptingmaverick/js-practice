b32a550b6b6c67a848e38f2c74d7bac1aef2019d
khasim <khasshaik12@gmail.com>
2025-12-28 01:01:04 +0530

    adds logic for day 15 part 1 & 2

--- adventOfCode/2020/day15_rambunctions/rambunction.js
+++ adventOfCode/2020/day15_rambunctions/rambunction.js
@@ -1,6 +1,39 @@
-const main = () => {
-  const input = Deno.readTextFileSync('input.txt').split('\n')
-  return input
-}
+const runGame = (initialSet, limit = 2020) => {
+  const numbers = new Int32Array(limit).fill(0);
+  let currTurn = 1;
+  for (let i = 0; i < initialSet.length - 1; i++) {
+    numbers[initialSet[i]] = i + 1;
+    currTurn++;
+  }
 
-console.log(main())
+  let lastNum = initialSet.at(-1);
+  while (currTurn < limit) {
+    const prevSeenTurn = numbers[lastNum];
+    numbers[lastNum] = currTurn;
+    const nextNum = prevSeenTurn === 0 ? 0 : currTurn - prevSeenTurn;
+    lastNum = nextNum;
+    currTurn++;
+  }
+
+  return lastNum;
+};
+
+const main = () => {
+  const input = Deno.readTextFileSync("input.txt").split(",").map(Number);
+  const examples = [
+    [0, 3, 6],
+    [1, 3, 2],
+    [2, 1, 3],
+    [1, 2, 3],
+    [2, 3, 1],
+    [3, 2, 1],
+    [3, 1, 2],
+  ];
+
+  return runGame(input, 30000000);
+};
+
+console.log(main());
+
+//p-1 -> 421
+// p-2 -> 436
