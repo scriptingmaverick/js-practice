66eef7fcb5aae6c8086f23635cacd83aec44a300
khasim <khasshaik12@gmail.com>
2026-01-01 11:33:26 +0530

    adds logic for whole orchestration

--- async/orchestration_prototype/main.js
+++ async/orchestration_prototype/main.js
@@ -1,32 +1,72 @@
+const contentsOf = (file) =>
+  Deno.readTextFile(file + ".txt").then((x) => x.split(",\r\n"));
+
+const parseNameIn = (data) => data.slice(data.lastIndexOf("/") + 1);
+
+const delay = (time) => new Promise((res) => setTimeout(res, time));
+
+const startOf = (task) => console.log("\nstarted ->", task);
+const endOf = (task) => console.log("ended ->", task);
+
 const execute = async (tasks) => {
   const log = [];
   const type = tasks.length === 1 ? "serial" : "parallel";
   const p = tasks.map(async (task) => {
+    const taskName = parseNameIn(task);
     const start = Date.now();
-    console.log(task, " started");
-    await new Promise((res) => setTimeout(res, 500));
+
+    startOf(taskName);
+    await delay(500);
+
     const data = await Deno.readTextFile(task + ".txt");
     const end = Date.now();
-    console.log(task, "ended");
     log.push({ type, msg: data.trim(), duration: end - start });
+
+    endOf(taskName);
   });
 
   await Promise.all(p);
   return log;
 };
 
-const runAll = async (recipeFile) => {
-  const recipe = await Deno.readTextFile(recipeFile + ".txt");
-  const tasks = recipe.split(",\r\n");
+const cookRecipe = async (recipeFile) => {
+  const recipeName = parseNameIn(recipeFile);
+  startOf(recipeName);
+
+  const tasks = await contentsOf(recipeFile);
   const mainLog = [];
+
   for (const task of tasks) {
     mainLog.push(await execute(task.split(",")));
-    await new Promise((res) => setTimeout(res, 100));
+    await new Promise((res) => setTimeout(res, 500));
   }
 
-  const recipeName = recipeFile.slice(recipeFile.lastIndexOf("/") + 1);
   console.log(mainLog.flat());
-  return Promise.resolve(`\nDone Recipe -> ${recipeName}.`);
+  return Promise.resolve(true);
 };
 
-console.log(await runAll(`./recipes/recipe1`));
+const getSubAndType = (result) =>
+  result.length > 1 ? ["recipes", "parallelly"] : ["recipe", "serially"];
+
+const returnFeed = (result) => {
+  const [subject, type] = getSubAndType(result);
+  console.log(subject, "done", type, "\n");
+};
+
+const runManifest = async (manifestFile) => {
+  const manifests = await contentsOf(manifestFile);
+
+  for (const manifest of manifests) {
+    const recipes = manifest.split(",");
+    const result = await Promise.all(
+      recipes.map((recipe) => cookRecipe(recipe))
+    );
+
+    returnFeed(result);
+    await delay(1000);
+  }
+
+  console.log(`\nDone manifest.`);
+};
+
+runManifest("./manifest");
