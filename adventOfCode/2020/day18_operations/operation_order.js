3e36d3f36e578e3aa35ebe0b7f78ac58d0d96393
khasim <khasshaik12@gmail.com>
2026-01-06 00:03:53 +0530

    adds some logic for part 1

--- adventOfCode/2020/day18_operations/operation_order.js
+++ adventOfCode/2020/day18_operations/operation_order.js
@@ -1,20 +1,67 @@
-const fn = (exp) => {
-  let [res, op1] = [0, 0];
-  while (exp.length > 0) {
-    const elem = exp.shift();
-    console.log(elem, op1, res, exp);
-    if (elem === "+" || elem === "*") {
-      op1 = eval(`${op1}${elem}${+exp.shift()}`);
-      res = op1;
-      continue;
-    }
-    op1 = +elem;
-    console.log("changed -> ", op1, res);
-  }
-
-  return res;
+const perform = (operator, operands) => {
+  const [op1, op2] = operands.splice(0, 2);
+  operands.splice(0, 0, eval(`${op1}${operator}${op2}`));
 };
 
-const exmp = "1 + 2 * 3 + 4 * 5 + 6";
+const evaluate = (operators, operands) => {
+  for (const operator of operators) {
+    perform(operator, operands);
+  }
 
-console.log("res -> ", fn(exmp.split(/ /)));
+  return operands;
+};
+
+const execute = (expression) => {
+  console.log("exp -> ", expression);
+  const stack = [];
+  let top = 0;
+  const operands = [];
+  const operators = { "+": true, "*": true, "(": true };
+  while (expression.length > 0) {
+    const elem = expression.shift();
+    if (elem in operators) {
+      stack.push(elem);
+      continue;
+    }
+    if (elem === ")") {
+      let lastOperator = stack.at(-1);
+      while (lastOperator !== "(") {
+        console.log("before -> ", operands);
+        perform(lastOperator, operands.reverse());
+        operands.reverse();
+        console.log("after -> ", operands);
+        stack.pop();
+        top--;
+        lastOperator = stack.at(-1);
+      }
+      stack.pop();
+      top--;
+      continue;
+    }
+
+    operands.push(elem);
+  }
+
+  console.log("stack -> ", stack);
+  console.log("operands -> ", operands);
+  return evaluate(stack, operands);
+};
+
+const exmp1 = "1 + 2 * 3 + 4 * 5 + 6";
+const exmp2 = "1 + (2 * (3 + 4) * 5) + 6";
+
+const parse = (exp) =>
+  exp
+    .split(/[\(\)]/)
+    .map((x) => x.split(" ").filter((x) => x !== ""))
+    .filter((x) => x.length > 0);
+
+const accurateParse = (exp) =>
+  exp
+    .split("(")
+    .map((x) => x.split(")"))
+    .map((x) => x.flatMap((y) => y.split(" ").filter((x) => x !== "")));
+
+console.log(execute(exmp2.split("").filter((x) => x !== " ")));
+
+// console.log("res -> ", evaluate(accurateParse(exmp1)[0]));
