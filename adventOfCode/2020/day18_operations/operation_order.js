0a9aefb73cb6a2fe36fa00efb9c294a099e2dca6
Khasim Sheik <khasimsheik@Khasims-MacBook-Pro.local>
2026-01-07 09:58:22 +0530

    makes some progress.

--- adventOfCode/2020/day18_operations/operation_order.js
+++ adventOfCode/2020/day18_operations/operation_order.js
@@ -1,67 +1,72 @@
 const perform = (operator, operands) => {
   const [op1, op2] = operands.splice(0, 2);
+  console.log(op1, operator, op2);
   operands.splice(0, 0, eval(`${op1}${operator}${op2}`));
+  console.log("operands after ope ->", operands);
 };
 
-const evaluate = (operators, operands) => {
-  for (const operator of operators) {
-    perform(operator, operands);
+const handleOpenParenthesis = (expression, operands) => {
+  let openCount = 1;
+  let i = 0;
+  while (openCount !== 0) {
+    const elem = expression[i++];
+    if (elem === "(") openCount++;
+    if (elem === ")") openCount--;
   }
 
-  return operands;
+  return execute(expression.splice(0, i));
 };
 
 const execute = (expression) => {
-  console.log("exp -> ", expression);
+  console.log("func called with exp -> ", expression);
+
   const stack = [];
-  let top = 0;
   const operands = [];
-  const operators = { "+": true, "*": true, "(": true };
+  const operators = { "+": true, "*": true };
+  let result = 0;
   while (expression.length > 0) {
     const elem = expression.shift();
-    if (elem in operators) {
-      stack.push(elem);
-      continue;
-    }
-    if (elem === ")") {
-      let lastOperator = stack.at(-1);
-      while (lastOperator !== "(") {
-        console.log("before -> ", operands);
-        perform(lastOperator, operands.reverse());
-        operands.reverse();
-        console.log("after -> ", operands);
-        stack.pop();
-        top--;
-        lastOperator = stack.at(-1);
-      }
-      stack.pop();
-      top--;
+    if (elem === "(") {
+      console.log("handling ( -> ", elem);
+      result = handleOpenParenthesis(expression, operands);
+      console.log("cursor backed with ", result);
       continue;
     }
 
+    if (elem === ")") {
+      console.log("handling ) ", result);
+      operands.push(result);
+      continue;
+    }
+
+    if (elem in operators) {
+      console.log("handling +,* ope ", elem);
+      stack.push(elem);
+      continue;
+    }
+
+    console.log("handling operands ", elem);
     operands.push(elem);
   }
 
   console.log("stack -> ", stack);
   console.log("operands -> ", operands);
-  return evaluate(stack, operands);
+  while (operands.length > 1) {
+    perform(stack.shift(), operands);
+  }
+
+  return operands[0];
 };
 
 const exmp1 = "1 + 2 * 3 + 4 * 5 + 6";
 const exmp2 = "1 + (2 * (3 + 4) * 5) + 6";
+const ex = [
+  "2 * 3 + (4 * 5)", // 26.
+  "5 + (8 * 3 + 9 + 3 * 4 * 3)", // 437.
+  "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", // 12240.
+  "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", // 13632.
+];
 
-const parse = (exp) =>
-  exp
-    .split(/[\(\)]/)
-    .map((x) => x.split(" ").filter((x) => x !== ""))
-    .filter((x) => x.length > 0);
-
-const accurateParse = (exp) =>
-  exp
-    .split("(")
-    .map((x) => x.split(")"))
-    .map((x) => x.flatMap((y) => y.split(" ").filter((x) => x !== "")));
-
-console.log(execute(exmp2.split("").filter((x) => x !== " ")));
+console.log(execute(ex[3].split("").filter((x) => x !== " ")));
 
 // console.log("res -> ", evaluate(accurateParse(exmp1)[0]));
