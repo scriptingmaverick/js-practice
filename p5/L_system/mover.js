0c7e9052721fadb40ef8ad4918c69604f22b5452
Khasim Sheik <khasimsheik@Khasims-MacBook-Pro.local>
2026-01-29 18:10:26 +0530

    adds something

---
+++ p5/L_system/mover.js
@@ -0,0 +1,12 @@
+class Mover{
+  constructor(x, y, angle) {
+    this.pos = createVector(x, y);
+    this.theta = angle
+    this.delta = 4;
+    this.states = []
+  }
+
+  move() {
+    const direction = 
+  }
+}
---
+++ p5/L_system/sketch.js
@@ -0,0 +1,9 @@
+/// <reference path="https://cdn.jsdelivr.net/npm/@types/p5/global.d.ts" />
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
