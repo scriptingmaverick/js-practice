8eb862e484caaa865efd437b7a18495bdf9867e2
Khasim Sheik <khasimsheik@Khasims-MacBook-Pro.local>
2026-02-03 11:25:49 +0530

    Adds logic for sending response to the request sender

--- requests/server.js
+++ requests/server.js
@@ -8,6 +8,15 @@ const parseHeaders = (data) => {
   return headers;
 };
 
+const formatHeaders = (headers) => {
+  const data = [];
+  for (const key in headers) {
+    data.push(`${key}: ${headers[key]}`);
+  }
+
+  return data.join("\r\n");
+};
+
 const parse = (data) => {
   const lines = data.split("\r\n");
   const requestLine = lines[0];
@@ -18,6 +27,9 @@ const parse = (data) => {
   return [requestLine, parseHeaders(headers), body];
 };
 
+const format = (responseLine, headers, body) =>
+  [responseLine, headers, "", body].join("\r\n");
+
 const establishServer = (port = 8000) =>
   Deno.listen({ port: 8000, transport: "tcp" });
 
@@ -27,20 +39,68 @@ const decoder = new TextDecoder();
 const encode = (text) => encoder.encode(text);
 const decode = (text) => decoder.decode(text);
 
-const handleRequest = async (conn) => {
+const successResponse = (body) => {
+  const responseLine = ["http/1.1", "200", "OK"].join(" ");
+  const headers = {
+    "content-type": "text/html",
+    "content-length": body.length,
+    "time": new Date(),
+  };
+
+  return [responseLine, formatHeaders(headers), body];
+};
+
+const failedResponse = () => {
+  const responseLine = ["http/1.1", "404", "Not Found"].join(" ");
+  const headers = {
+    "content-type": "text/html",
+    "time": new Date(),
+  };
+
+  return [
+    responseLine,
+    formatHeaders(headers),
+    Deno.readTextFileSync("./error.html"),
+  ];
+};
+
+const parseReqLine = (reqLine) => reqLine.split(" ");
+
+const pages = {
+  "/news": "./news.html",
+  "/fruits": "./fruits.html",
+};
+
+const handleRequest = (requestLine, headers, body) => {
+  const [method, request_path, protocol] = parseReqLine(requestLine);
+
+  if (request_path === "/") {
+    const data = Deno.readTextFileSync("welcome.html");
+    return format(...successResponse(data));
+  } else if (request_path in pages) {
+    const data = Deno.readTextFileSync(pages[request_path]);
+    return format(...successResponse(data));
+  } else {
+    return format(...failedResponse(), "");
+  }
+};
+
+const handleConn = async (conn) => {
   const buffer = new Uint8Array(1024);
   const n = await conn.read(buffer);
   const data = decode(buffer.subarray(0, n));
 
   const [requestLine, headers, body] = parse(data);
-  console.log({ requestLine, headers, body });
+  const response = handleRequest(requestLine, headers, body);
+
+  await conn.write(encode(response));
   await conn.close();
 };
 
 const main = async () => {
   const server = establishServer();
   for await (const conn of server) {
-    handleRequest(conn);
+    await handleConn(conn);
   }
 };
 
